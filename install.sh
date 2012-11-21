f0aa53122ff314241311e5703806e6d8ef124a70
BLYTH3 <ST4R13UCK@gmail.com>
2012-11-21 15:38:32 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,47 +3,50 @@
 #default environment alfresco
 # * is the your alfresco version
 
+#find alfreso
+FINDALF=`ls /opt/ | grep alfresco`
+
 #create audit folder
-mkdir -p /opt/alfresco*/tomcat/webapps/share/components/audit
-mkdir -p /opt/alfresco*/tomcat/webapps/share/WEB-INF/classes/alfresco/site-webscripts/org/alfresco/components/audit
+mkdir -p /opt/$FINDALF/tomcat/webapps/share/components/audit
+mkdir -p /opt/$FINDALF/tomcat/webapps/share/WEB-INF/classes/alfresco/site-webscripts/org/alfresco/components/audit
 #mkdir /opt/alfresco*/tomcat/shared/classes/alfresco/extension/audit
 
 #copy all application and config
-PATHAPP='/opt/alfresco*/tomcat/shared/classes/alfresco/extension/'
+PATHAPP='/opt/$FINDALF/tomcat/shared/classes/alfresco/extension/'
 cp -rf tomcat/shared/classes/alfresco/extension/* ${PATHAPP}
 echo "Application and config  [success]"
 
 #copy all file into webapps/share/components
-PATHCM='/opt/alfresco*/tomcat/webapps/share/components/audit/'
+PATHCM='/opt/$FINDALF/tomcat/webapps/share/components/audit/'
 cp tomcat/webapps/share/components/audit/* ${PATHCM}
 echo "Components and javascrip  [success]"
 
 #capy all image into images of alfresco
-PATHIMG='/opt/alfresco*/tomcat/webapps/share/components/images/'
+PATHIMG='/opt/$FINDALF/tomcat/webapps/share/components/images/'
 cp tomcat/webapps/share/components/audit/images/* ${PATHIMG}
 echo "Images [success]"
 
 #copy all page
-PATHPAGE='/opt/alfresco*/tomcat/webapps/share/WEB-INF/classes/alfresco/site-data/pages/'
+PATHPAGE='/opt/$FINDALF/tomcat/webapps/share/WEB-INF/classes/alfresco/site-data/pages/'
 cp tomcat/webapps/share/WEB-INF/classes/alfresco/site-data/pages/* ${PATHPAGE}
 echo "Page [success]"
 
 #copy title
-PATHTI='/opt/alfresco*/tomcat/webapps/share/WEB-INF/classes/alfresco/site-webscripts/org/alfresco/components/title/'
+PATHTI='/opt/$FINDALF/tomcat/webapps/share/WEB-INF/classes/alfresco/site-webscripts/org/alfresco/components/title/'
 cp tomcat/webscripts/share/WEB-INF/classes/alfresco/site-webscripts/org/alfresco/components/title/* ${PATHTI}
 
 #copy all template-instances
-PATHTMEPLATE='/opt/alfresco*/tomcat/webapps/share/WEB-INF/classes/alfresco/site-data/template-instances/'
+PATHTMEPLATE='/opt/$FINDALF/tomcat/webapps/share/WEB-INF/classes/alfresco/site-data/template-instances/'
 cp tomcat/webapps/share/WEB-INF/classes/alfresco/site-data/template-instances/* ${PATHTMEPLATE}
 echo "Template-instances [success]"
 
 #copy all web-scripts
-PATHWEB='/opt/alfresco*/tomcat/webapps/share/WEB-INF/classes/alfresco/site-webscripts/org/alfresco/components/audit/'
+PATHWEB='/opt/$FINDALF/tomcat/webapps/share/WEB-INF/classes/alfresco/site-webscripts/org/alfresco/components/audit/'
 cp tomcat/webapps/share/WEB-INF/classes/alfresco/site-webscripts/org/alfresco/components/audit/* ${PATHWEB}
 echo "Site-webscripts [success]"
 
 #copy all freemarker template
-PATHTMP='/opt/alfresco*/tomcat/webapps/share/WEB-INF/classes/alfresco/templates/org/alfresco/'
+PATHTMP='/opt/$FINDALF/tomcat/webapps/share/WEB-INF/classes/alfresco/templates/org/alfresco/'
 cp tomcat/webapps/share/WEB-INF/classes/alfresco/templates/org/alfresco/* ${PATHTMP}
 echo "Freemarker template [success]"
 
